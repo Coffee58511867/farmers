dd67b768e2c26aa019b1c0d0d56c7e3526bc6e79
Coffee58511867 <marobahlalele910@gmail.com>
2022-09-08 14:32:27 +0200

    Added Register Function

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,44 +3,53 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Sell from "../Pages/Sell";
 import Market from "../Pages/Market";
 import { Route, Routes } from "react-router-dom";
 import Login from "../Pages/Login";
+import Register from "../Pages/Register";
 
 const Navba = () => {
-
-    const SignIn = () =>{
-        window.location.href = "/login"
-    }
-    const SellProduct = () =>{
-        window.location.href = "/sell"
-    }
+  const SignIn = () => {
+    window.location.href = "/login";
+  };
+  const SellProduct = () => {
+    window.location.href = "/register";
+  };
   return (
     <div>
-        <BrowserRouter>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Farmers</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">MarketPlace</Nav.Link>
-            <Nav.Link href="#features">Diseases</Nav.Link>
-            <Nav.Link href="#pricing">Fertilizers</Nav.Link>
-            <Nav.Link href="#pricing"> Weather</Nav.Link>
-          </Nav>
-        </Container>
-        
-        <Button variant="outline-primary" className="btn" onClick={SellProduct}>    
-        Sign Up</Button>{" "}
-        <Button variant="outline-success" onClick={SignIn}>Sign In</Button>{" "}
-      </Navbar>
+      <BrowserRouter>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">Farmers</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">MarketPlace</Nav.Link>
+              <Nav.Link href="#features">Diseases</Nav.Link>
+              <Nav.Link href="#pricing">Fertilizers</Nav.Link>
+              <Nav.Link href="#pricing"> Weather</Nav.Link>
+           
+            </Nav>
+          </Container>
+          <Button
+            variant="outline-primary"
+            className="sin"
+            onClick={SellProduct}
+          >
+            Sign Up
+          </Button>{" "}
+          <Button variant="outline-primary" onClick={SignIn}>
+            Sign In 
+          </Button>{" "}
+          
+        </Navbar>
 
-      <Routes>
-        <Route path="/sell" element={<Sell />} />
-        <Route path="/" element={<Market />} />
-        <Route path="/login" element={<Login/>} />
-      </Routes>
+        <Routes>
+          <Route path="/sell" element={<Sell />} />
+          <Route path="/" element={<Market />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register/>} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/Pages/Register.js
@@ -0,0 +1,104 @@
+import React, { useState } from "react";
+import { Form, Button, Toast } from "react-bootstrap";
+import axios from "axios";
+
+const Register = () => {
+    const [firstname,setFirstName] = useState(" ");
+    const [lastname,setLastName] = useState(" ");
+    const [phone,setPhone] = useState(" ");
+    const [address,setAddress] = useState(" ");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          const body = { firstname,email,password,lastname,phone,address};
+          const response = await fetch("http://localhost:8000/app/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body),
+          });
+          console.log(response);
+        } catch (err) {
+          console.log(err.message);
+        }
+      };
+  return (
+    <div>
+        <Form onSubmit={handleSubmit} className="Form">
+        <Form.Label>First Name</Form.Label>
+          <Form.Group className="mb-31">
+            <Form.Control
+              type="text"
+              className="form"
+              placeholder="Enter First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+              value={firstname}
+            />
+          </Form.Group>
+          <Form.Label>Last Name</Form.Label>
+          <Form.Group className="mb-31">
+            <Form.Control
+              type="text"
+              className="form"
+              placeholder="Enter Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastname}
+            />
+          </Form.Group>
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Group className="mb-31">
+            <Form.Control
+              type="number"
+              className="form"
+              placeholder="Enter Phone Number"
+              onChange={(e) => setPhone(e.target.value)}
+              value={phone}
+            />
+          </Form.Group>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Group className="mb-31">
+            <Form.Control
+              type="email"
+              className="form"
+              placeholder="Enter Email Adress"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+          </Form.Group>
+          <Form.Label>Address</Form.Label>
+          <Form.Group className="mb-31">
+            <Form.Control
+              type="text"
+              className="form"
+              placeholder="Enter Address"
+              onChange={(e) => setAddress(e.target.value)}
+              value={address}
+            />
+          </Form.Group>
+          <Form.Label>Password</Form.Label>
+          <Form.Group className="mb-31">
+            <Form.Control
+              type="password"
+              className="form"
+              placeholder="Enter Password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </Form.Group>
+
+          <Button
+            variant="primary"
+            className="button"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Register
+          </Button>
+        </Form>
+    </div>
+  )
+}
+
+export default Register
